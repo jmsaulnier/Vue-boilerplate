9e3a9606f6bc3ce3d45d96ea00140e1153ea1adc
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2015-04-29 17:17:15 -0700

    Clean old JS bundle

--- tasks/build.js
+++ tasks/build.js
@@ -8,8 +8,14 @@ var del         = require('del');
 gulp.task('build', ['clean'], function (cb) {
   global.dist = true; // changes Watchify's build destination
   // add `watchify` task to the run sequence (below)
-  runSequence('styles', ['jshint', 'watchify', 'html', 'images', 'fonts', 'copy'], 'compress', cb);
+  runSequence('styles', ['jshint', 'watchify', 'html', 'images', 'fonts', 'copy'], 'compress', 'clean:scripts', cb);
 });
 
 // Clean Output Directory
 gulp.task('clean', del.bind(null, ['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
+
+gulp.task('clean:scripts', function (cb) {
+  del([
+    'dist/scripts/bundle.js?v=@@version'
+  ], cb);
+});
